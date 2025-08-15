c6a9a7aabff621b2b19fab18c01a7a1183376b49
Kovács Péter <smitpeter777@gmail.com>
2025-08-15 22:39:00 +0200

    add

--- app/Filament/Widgets/TestChartWidget.php
+++ app/Filament/Widgets/TestChartWidget.php
@@ -2,17 +2,51 @@
 
 namespace App\Filament\Widgets;
 
+use App\Models\User;
+use Filament\Facades\Filament;
 use Filament\Widgets\ChartWidget;
+use Flowframe\Trend\Trend;
+use Flowframe\Trend\TrendValue;
 
 class TestChartWidget extends ChartWidget
 {
-    protected static ?string $heading = 'Chart';
+    protected static ?string $heading = 'Test Chart Widget';
 
     protected function getData(): array
     {
+
+        $data = Trend::model(User::class)
+            ->between(
+                start: now()->subMonth(1),
+                end: now(),
+            )
+            ->perDay()
+            ->count();
+
         return [
-            //
+            'datasets' => [
+                [
+                    'label' => 'Treatments',
+                    'data' => $data->map(fn (TrendValue $value) => $value->aggregate),
+                ],
+            ],
+            'labels' => $data->map(fn (TrendValue $value) => $value->date),
         ];
+
+        /*return [
+            'datasets' => [
+                [
+                    'label' => 'Blog posts created',
+                    'data' => [300, 500, 100],
+                    'backgroundColor' => [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                    ],
+                ],
+            ],
+            'labels' => ['A', 'B', 'C'],
+        ];*/
     }
 
     protected function getType(): string
